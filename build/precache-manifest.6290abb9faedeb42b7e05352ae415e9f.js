199ff7aa3ecf564f690a2600111ca8cf4ac1d5ff
afik <afikziv@gmail.com>
2021-09-11 13:50:39 +0300

    initial commit

---
+++ build/precache-manifest.6290abb9faedeb42b7e05352ae415e9f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f95947f8aa1f1268a1a8fad06af31967",
+    "url": "/index.html"
+  },
+  {
+    "revision": "267908ff37903e59c43c",
+    "url": "/static/js/2.82cd775c.chunk.js"
+  },
+  {
+    "revision": "6851195ef5830af7bcfa",
+    "url": "/static/js/main.5bf855c6.chunk.js"
+  },
+  {
+    "revision": "0d18ab6c7c1f4f9c802e",
+    "url": "/static/js/runtime-main.6c0f1b0f.js"
+  }
+]);
